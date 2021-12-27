016957cc975a97f42a27f31ffc173cab5c0aac2e
indigolain <indigo.lain@gmail.com>
2021-12-27 14:27:39 +0900

    Finish second module

--- bitonic-sorter/src/second.rs
+++ bitonic-sorter/src/second.rs
@@ -1,8 +1,22 @@
-pub fn sort<T: Ord>(x: &mut [T], up: bool) {
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order:&SortOrder) -> Result<(), String>{
+    if x.len().is_power_of_two() {
+        match *order {
+          SortOrder::Ascending => do_sort(x, true),
+          SortOrder::Descending => do_sort(x, false),
+        };
+        Ok(())
+    } else {
+        Err(format!("The length of x is not a power of two. (x.len(): {})", x.len()))
+    }
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
         sub_sort(x, up);
     }
 }
@@ -28,18 +42,19 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 #[cfg(test)]
 mod tests {
     use super::sort;
+    use crate::SortOrder::*;
 
     #[test]
     fn sort_u32_ascending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, true);
+        assert!(sort(&mut x, &Ascending).is_ok());
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
     #[test]
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, false);
+        assert!(sort(&mut x, &Descending).is_ok());
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
 
@@ -55,7 +70,7 @@ mod tests {
             "no",
             "GC",
         ];
-        sort(&mut x, true);
+        assert!(sort(&mut x, &Ascending).is_ok());
         assert_eq!(
             x,
             vec![
@@ -83,7 +98,7 @@ mod tests {
             "no",
             "GC",
         ];
-        sort(&mut x, false);
+        assert!(sort(&mut x, &Descending).is_ok());
         assert_eq!(
             x,
             vec![
@@ -99,6 +114,12 @@ mod tests {
         );
     }
 
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10,30,11];
+        assert!(sort(&mut x, &Ascending).is_err());
+    }
+
     // #[test]
     // fn sort_f64() {
     //     let mut x = vec![20.0, -30.0, 11.0, 10.0];
@@ -109,7 +130,7 @@ mod tests {
     // fn sort_mixed() {
     //     if false {
     //         let mut x = vec![10, 30, "a", "b"];
-    //         sort(&mut x, true);
+    //         sort(&mut x, &Ascending);
     //     }
     // }
 }
