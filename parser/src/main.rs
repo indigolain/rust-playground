fb7e102635d38aaebba2868e5f312b04b5888de6
indigolain <indigo.lain@gmail.com>
2021-12-28 16:58:48 +0900

    RPN compiler

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -603,6 +603,7 @@ fn print_annot(input: &str, loc: Loc) {
 fn main() {
     use std::io::{stdin, BufRead, BufReader};
     let mut interp = Interpreter::new();
+    let mut compiler = RpnCompiler::new();
 
     let stdin = stdin();
     let stdin = stdin.lock();
@@ -620,6 +621,8 @@ fn main() {
                     continue;
                 }
             };
+            let rpn = compiler.compile(&ast);
+            println!("compiled in rpn: {}", rpn);
             let n = match interp.eval(&ast) {
                 Ok(n) => n,
                 Err(e) => {
@@ -634,6 +637,53 @@ fn main() {
         }
     }
 }
+
+
+struct RpnCompiler;
+impl RpnCompiler {
+    pub fn new() -> Self {
+        RpnCompiler
+    }
+    pub  fn compile(&mut self, expr: &Ast) -> String {
+        let mut buf = String::new();
+        self.compile_inner(expr, &mut buf);
+        buf
+    }
+    pub fn compile_inner(&mut self, expr: &Ast, buf: &mut String) {
+        use self::AstKind::*;
+        match expr.value {
+            Num(n) => buf.push_str(&n.to_string()),
+            UniOp { ref op, ref e} => {
+                self.compile_uniop(op, buf);
+                self.compile_inner(e, buf)
+            }
+            BinOp { ref op, ref l,ref r, } => {
+                self.compile_inner(l, buf);
+                buf.push_str(" ");
+                self.compile_inner(r, buf);
+                buf.push_str(" ");
+                self.compile_binop(op, buf)
+            }
+        }
+    }
+    fn compile_uniop(&mut self, op: &UniOp, buf: &mut String) {
+        use self::UniOpKind::*;
+        match op.value {
+            Plus => buf.push_str("+"),
+            Minus => buf.push_str("-"),
+        }
+    }
+    fn compile_binop(&mut self, op: &BinOp, buf: &mut String) {
+        use self::BinOpKind::*;
+        match op.value {
+            Add => buf.push_str("+"),
+            Sub => buf.push_str("-"),
+            Mult => buf.push_str("*"),
+            Div => buf.push_str("/"),
+        }
+    }
+}
+
 
 #[test]
 fn test_lexer() {
